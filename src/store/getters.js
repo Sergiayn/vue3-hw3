8324f2cdd3a13f8e2066c9b70f688837675eb4db
Sergey <SergeySm128@gmail.com>
2021-01-27 19:38:21 +0200

    show task

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,14 @@
-import state from "@/store/state"
 import formatDate from "@/use/formatDate"
 
 export default {
-    countActiveStatus() {
+    countActiveStatus(state) {
         const reducer = (accumulator, item) => item.status === "active" ? accumulator + 1 : accumulator
         return state.tasks.reduce(reducer, 0)
     },
-    tasks (){
+    task: (state) => (id) => {
+        return state.tasks.find(task => task.id === id)
+    },
+    tasks (state){
         return state.tasks.filter(item => {
             item.date = formatDate(item.timestamp)
             return item
--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,28 +1,37 @@
 <template>
-  <div class="card">
-    <h2>Название задачи</h2>
-    <p><strong>Статус</strong>: <AppStatus :type="'done'" /></p>
-    <p><strong>Дэдлайн</strong>: {{ new Date().toLocaleDateString() }}</p>
-    <p><strong>Описание</strong>: Описание задачи</p>
+  <div class="card" v-if="task">
+    <h2>{{task.name}}</h2>
+    <p><strong>Статус</strong>: <AppStatus :type="task.status" /></p>
+    <p><strong>Дэдлайн</strong>: {{ new Date(task.timestamp * 1000).toLocaleDateString() }}</p>
+    <p><strong>Описание</strong>: {{task.desc}}</p>
     <div>
       <button class="btn">Взять в работу</button>
       <button class="btn primary">Завершить</button>
       <button class="btn danger">Отменить</button>
     </div>
   </div>
-  <h3 class="text-white center">
-    Задачи с id = <strong>Tут АЙДИ</strong> нет.
+  <h3 class="text-white center" v-else>
+    Задачи с id = <strong>{{id}}</strong> нет.
   </h3>
 </template>
 
 <script>
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
 import AppStatus from '../components/AppStatus'
 
 export default {
-  components: {AppStatus}
+  components: {AppStatus},
+  setup() {
+    const route = useRoute()
+    const store = useStore()
+    const id = parseInt(route.params.taskId)
+    const task = store.getters.task(id)
+
+    return {
+      id,
+      task
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
